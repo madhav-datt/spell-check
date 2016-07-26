e8cbc2139e88cd183d4e9b5329bd515ea7ca0ad5
madhav-datt <madhav.datt@hotmail.com>
2016-07-26 17:35:33 +0530

    add text extraction from pdfs

--- pdf_text/extraction.py
+++ pdf_text/extraction.py
@@ -20,10 +20,11 @@ from __future__ import print_function
 page_time = 0.457
 default_kill_time = 30
 
-def convert_single_file(file):
+def convert_single_file(directory, file):
     """
     Extract text from passed PDF file to text file in the same directory
-    :param directory: Path to PDF file to be converted. Only absolute paths/hard links only
+    :param directory: Path to directory containing PDFs to be converted. Only absolute paths/hard links only
+    :param file: Path to PDF file to be converted. Only absolute paths/hard links only
     """
 
     # Create name for file as "pdf_name.txt"
@@ -39,8 +40,8 @@ def convert_single_file(file):
     # Convert PDF to txt with call to pdf2txt.py script
     cmd_pdf2txt = "python2.7 src/to_text/pdf2txt.py -o {directory_path}/{txt_file_name} " \
                   "-t text {pdf_file_name} 2>/dev/null".format(directory_path=directory,
-                                                                       txt_file_name=file_name,
-                                                                       pdf_file_name=pdf_file_path)
+                                                               txt_file_name=file_name,
+                                                               pdf_file_name=pdf_file_path)
 
     # Set kill-time limit based on number of pages
     cmd_num_page = "pdfinfo {pdf_file_name} | grep 'Pages' | awk '{statement}' 2>/dev/null".format(
@@ -69,6 +70,7 @@ def convert_single_file(file):
     except Exception:
         pass
 
+    return txt_file_path
 
 def pdf_to_txt(directory):
     """
@@ -77,49 +79,9 @@ def pdf_to_txt(directory):
     """
 
     for file in listdir(directory):
-        if isfile(join(directory, file)) and re.match("^.*[\.]pdf$", file):
-            # Create name for file as "pdf_name.txt"
-            file_name = file[0:len(file) - 4] + ".txt"
-            pdf_file_path = join(directory, file)
-            txt_file_path = join(directory, file_name)
-
-            # Skip files if "pdf_name.txt" already exists
-            # Assume previous conversion attempt successful
-            if isfile(txt_file_path) and stat(txt_file_path).st_size > 0:
-                continue
-
-            # Convert PDF to txt with call to pdf2txt.py script
-            cmd_pdf2txt = "python2.7 src/to_text/pdf2txt.py -o {directory_path}/{txt_file_name} " \
-                          "-t text {pdf_file_name} 2>/dev/null".format(directory_path=directory,
-                                                                       txt_file_name=file_name,
-                                                                       pdf_file_name=pdf_file_path)
-
-            # Set kill-time limit based on number of pages
-            cmd_num_page = "pdfinfo {pdf_file_name} | grep 'Pages' | awk '{statement}' 2>/dev/null".format(
-                pdf_file_name=pdf_file_path,
-                statement="{print $2}")
-
-            try:
-                kill_time = ceil(int(popen(cmd_num_page).read().strip()) * page_time)
-            except ValueError:
-                kill_time = default_kill_time
-
-            try:
-                # Run command on separate thread with call to system as a process
-                converter_process = Process(target=system, name="pdf2txt_convert", args=(cmd_pdf2txt,))
-                converter_process.start()
-
-                # Wait a maximum of kill_time seconds for system command with call to script to execute
-                converter_process.join(kill_time)
-
-                # Terminate thread if active
-                # Image based/without text layer conversion will be terminated
-                if converter_process.is_alive():
-                    converter_process.terminate()
-                    converter_process.join()
-                    system("rm -f {txt_file_name}".format(txt_file_name=file_name))
-            except Exception:
-                pass
+        directory_file_path = join(directory, file)
+        if isfile(directory_file_path) and re.match("^.*[\.]pdf$", file):
+            convert_single_file(directory_file_path)
 
 
 if __name__ == '__main__':
@@ -142,7 +104,13 @@ if __name__ == '__main__':
 
     # Individual file spellcheck mode
     elif isfile(file_path):
-        pass
+        file_extension = file_path[-4:]
+        if file_extension == ".pdf":
+            files_list = [convert_single_file("/", file_path)]
+        elif file_extension == ".txt":
+            files_list = [file_path]
+        else:
+            sys.exit()
 
     chdir("src/")
     system("make")
@@ -150,5 +118,3 @@ if __name__ == '__main__':
         cmd_autocorrect = "./spellcheck {check_file}".format(check_file=file)
         print(file)
         system(cmd_autocorrect)
-
-    sys.exit()
